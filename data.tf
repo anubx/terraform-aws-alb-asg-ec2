1a4c631ac723e7930eb3d2a56e0b68e806f32c55
nxops <92003229+anubx@users.noreply.github.com>
2023-02-02 10:55:11 -0500

    Update data.tf

--- data.tf
+++ data.tf
@@ -2,11 +2,11 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_availability_zones" "azs" {
-  all_availability_zones = true
+#   all_availability_zones = true
 
   filter {
     name   = "opt-in-status"
-    values = ["not-opted-in", "opted-in"]
+    values = ["opted-in"]
   }
 }
 
